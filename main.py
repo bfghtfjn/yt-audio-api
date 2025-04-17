9a0af7299ce949a8e5ed8da618d0212a5bae273c
bfghtfjn <60049767+bfghtfjn@users.noreply.github.com>
2025-04-17 23:26:59 +0300

    Upde main.py

--- main.py
+++ main.py
@@ -11,6 +11,10 @@ from constants import *
 app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
+def home():
+    return "YouTube to MP3 API is running!"
+
+@app.route("/yt", methods=["GET"])
 def handle_audio_request():
     video_url = request.args.get("url")
     if not video_url:
@@ -28,14 +32,14 @@ def handle_audio_request():
             'preferredquality': '192'
         }],
         'quiet': True,
-        'cookiefile': 'cookies.txt'
+        'cookiefile': 'cookies.txt'  # لازم تكون مرفوعة بالملف
     }
 
     try:
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             ydl.download([video_url])
     except Exception as e:
-        return jsonify(error="Failed to download or convert audio.", detail=str(e)), INTERNAL_SERVER_ERROR
+        return jsonify(error="Download failed", detail=str(e)), INTERNAL_SERVER_ERROR
 
     return _generate_token_response(filename)
 
@@ -43,19 +47,19 @@ def handle_audio_request():
 def download_audio():
     token = request.args.get("token")
     if not token:
-        return jsonify(error="Missing 'token' parameter in request."), BAD_REQUEST
+        return jsonify(error="Missing token"), BAD_REQUEST
 
     if not access_manager.has_access(token):
-        return jsonify(error="Token is invalid or unknown."), UNAUTHORIZED
+        return jsonify(error="Invalid token"), UNAUTHORIZED
 
     if not access_manager.is_valid(token):
-        return jsonify(error="Token has expired."), REQUEST_TIMEOUT
+        return jsonify(error="Token expired"), REQUEST_TIMEOUT
 
     try:
         filename = access_manager.get_audio_file(token)
         return send_from_directory(ABS_DOWNLOADS_PATH, filename=filename, as_attachment=True)
     except FileNotFoundError:
-        return jsonify(error="Requested file could not be found on the server."), NOT_FOUND
+        return jsonify(error="File not found"), NOT_FOUND
 
 def _generate_token_response(filename: str):
     token = secrets.token_urlsafe(TOKEN_LENGTH)
@@ -70,6 +74,6 @@ def main():
     token_cleaner_thread.start()
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get('PORT', 10000))
     main()
     app.run(host="0.0.0.0", port=port)
